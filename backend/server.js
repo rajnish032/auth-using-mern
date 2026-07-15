6753e3613db755401ef3bf4a681cc70b6e7d859c
Rajnish Kumar <rajnishmaurya250@gmail.com>
2026-07-15 20:03:50 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://authusingmern.netlify.app', process.env.CLIENT_URL].filter(Boolean),
     credentials: true,
 }));
 
